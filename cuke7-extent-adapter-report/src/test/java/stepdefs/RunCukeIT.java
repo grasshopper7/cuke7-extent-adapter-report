e633fb6cfe3ad0ecafddcf031d6a6c938857fef9
grasshopper7 <grass.hopper.moc@gmail.com>
2023-05-05 22:44:16 +0530

    Update version

--- cuke7-extent-adapter-report/src/test/java/stepdefs/RunCukeIT.java
+++ cuke7-extent-adapter-report/src/test/java/stepdefs/RunCukeIT.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/stepdefs/", plugin = {
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "summary"
-/* "rerun:target/rerun1.txt" */ }/* , tags = "@skip or @exception or @both or @duplicate" */)
+/* "rerun:target/rerun1.txt" */ }, tags = "@skip or @exception or @both or @duplicate")
 public class RunCukeIT {
 
 	/*
